179a5afc9f9a2be62fb99b845aadbff511c3a6b8
user_caps <user_caps@mail.ru>
2020-05-14 12:19:34 +0300

    js config

--- js/scipt.js
+++ js/scipt.js
@@ -11,7 +11,7 @@
       if(document.querySelectorAll('.owl-dot')[0].className === 'owl-dot active'){
          document.querySelector('.container1').classList.add('container1_img1');
          document.querySelector('.container1').classList.remove('container1_img2');
-      }else if((document.querySelectorAll('.owl-dot')[1].className === 'owl-dot active'){
+      }else if((document.querySelectorAll('.owl-dot')[1].className === 'owl-dot active')){
          document.querySelector('.container1').classList.remove('container1_img1');
          document.querySelector('.container1').classList.add('container1_img2');
       }
